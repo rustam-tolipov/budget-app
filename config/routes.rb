21d24a820a4821d8673406b6b962f15c12e472d6
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-11 14:50:22 +0500

    Display user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  root to: 'users#show'
+
+  resources :users, only: [:show]
 end

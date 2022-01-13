267a3c861fa2b9c853d0fd085a6bd6eb5db56061
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-13 11:43:02 +0500

    Update income for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :income])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :income])
   end
 end

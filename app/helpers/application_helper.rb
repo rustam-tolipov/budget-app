e02ef740e9ecec846911c99d19fcd750ea16ef77
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-14 11:08:33 +0500

    Update default image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def avatar_url(user)
-    user.avatar.attached? ? user.avatar.variant(resize: '300x300') : 'tom.jpeg'
+    user.avatar.attached? ? user.avatar.variant(resize: '300x300') : 'https://www.w3schools.com/howto/img_avatar.png'
   end
 end

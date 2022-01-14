0c8fd288671132a8fb8c74388f18a9e478157b13
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-14 21:49:21 +0500

    Update validation for avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :groups, dependent: :destroy
   has_many :entities, dependent: :destroy
-
-  has_one_attached :avatar
+  has_one_attached :avatar, content_type: %w[image/jpeg image/png image/gif image/jpg]
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

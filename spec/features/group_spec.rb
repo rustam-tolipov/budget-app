d4be22bd9f8d8d6fcb2be659fb0def99f43c1c12
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-14 09:06:51 +0500

    Write test for group

---
+++ spec/features/group_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature 'Groups', type: :feature do
+  background do
+    visit new_user_registration_path
+    fill_in 'Name', with: 'Example User'
+    fill_in 'Email', with: 'tom@gmail.com'
+    fill_in 'Password', with: 'foobar'
+    fill_in 'Confirm Password', with: 'foobar'
+    click_button 'Sign Up'
+
+    visit groups_path
+  end
+
+  scenario 'show groups_path' do
+    expect(page).to have_content('Total Expenses')
+  end
+
+  scenario 'show user name' do
+    expect(page).to have_content('Example User')
+  end
+
+  scenario 'show user image' do
+    expect(page).to have_css('img')
+  end
+
+  scenario 'add a group' do
+    click_link 'Add'
+    expect(page).to have_content('Add Group')
+  end
+end
